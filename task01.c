960601e75c52501d0073ab335b3ed1dd8113717c
emlen <emlen.ra@gmail.com>
2020-06-07 17:40:55 +0300

    task result

--- task01.c
+++ task01.c
@@ -4,10 +4,10 @@ int main (void)
 	float sum, step;
 	sum=0;
 	step=0.5;
-	printf ("SUM=%f, STEP=%f\n", sum, step);
+	//printf ("SUM=%f, STEP=%f\n", sum, step);
 	while (sum<1)
 		{
-		printf ("%f, %f + %f =\n", sum, sum, step);
+		//printf ("%f, %f + %f =\n", sum, sum, step);
 		sum=sum+step;
 		step=step/2;
 		}
